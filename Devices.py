fbedb704a901d622422ed8d13459f5e277b126e1
Fireblade2534 <77405729+fireblade2534@users.noreply.github.com>
2024-10-07 16:17:19 +0000

    Added Slot Class

--- Devices.py
+++ Devices.py
@@ -18,6 +18,18 @@ class Pins:
             self.d4=d4
             self.d5=d5
 
+class Slot:
+     def __init__(self,Class:int=0,Damage:int=0,MaxQuantity:int=0,OccupantHash:int=0,Occupied:int=0,PrefabHash:int=0,Quantity:int=0,ReferenceId:int=0,SortingClass:int=0):
+          self.Class=Utility.Field(StartValue=Class,Read=True,Write=False)
+          self.Damage=Utility.Field(StartValue=Damage,Read=True,Write=False)
+          self.MaxQuantity=Utility.Field(StartValue=MaxQuantity,Read=True,Write=False)
+          self.OccupantHash=Utility.Field(StartValue=OccupantHash,Read=True,Write=False)
+          self.Occupied=Utility.Field(StartValue=Occupied,Read=True,Write=False)
+          self.PrefabHash=Utility.Field(StartValue=PrefabHash,Read=True,Write=False)
+          self.Quantity=Utility.Field(StartValue=Quantity,Read=True,Write=False)
+          self.ReferenceId=Utility.Field(StartValue=ReferenceId,Read=True,Write=False)
+          self.SortingClass=Utility.Field(StartValue=SortingClass,Read=True,Write=False)
+
 class StructureCircuitHousing(Device):
     
     def __init__(self, Name: str=None, ReferenceId: int = None,On:int=0,Power: int=1,RequiredPower:int=0,Setting:int=0,Pins:Pins=None):
