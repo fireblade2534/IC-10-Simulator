ccd29d9062e553bd1784b24eafe81a406eb15f8d
Fireblade2534 <77405729+fireblade2534@users.noreply.github.com>
2024-10-07 16:21:10 +0000

    Added stack to InternalState

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ class InternalState:
     def __init__(self):
         self.Registers={f"r{X}":0 for X in range(0,18)}
         self.RegisterAliases={"sp":16,"ra":17}
+        self.Stack=[0 for X in range(512)]
         self.Constants={}
         self.Devices={}
 
