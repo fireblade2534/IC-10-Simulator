092e831d279ac2866d573bdbb2412c1e12ef66b6
Fireblade2534 <77405729+fireblade2534@users.noreply.github.com>
2024-10-07 16:19:23 +0000

    Added Slots to StructureCircuitHousing class

--- Devices.py
+++ Devices.py
@@ -32,14 +32,14 @@ class Slot:
 
 class StructureCircuitHousing(Device):
     
-    def __init__(self, Name: str=None, ReferenceId: int = None,On:int=0,Power: int=1,RequiredPower:int=0,Setting:int=0,Pins:Pins=None):
+    def __init__(self, Name: str=None, ReferenceId: int = None,On:int=0,Power: int=1,RequiredPower:int=0,Setting:int=0,Pins:Pins=None,Slots:list[Slot]=[Slot()]):
         self.Error=Utility.Field(StartValue=0,Read=True,Write=False)
         self.LineNumber=Utility.Field(StartValue=0,Read=True,Write=True)
         self.On=Utility.Field(StartValue=On,Read=True,Write=True)
         self.Power=Utility.Field(StartValue=Power,Read=True,Write=False)
         self.RequiredPower=Utility.Field(StartValue=RequiredPower,Read=True,Write=False)
         self.Setting=Utility.Field(StartValue=Setting,Read=True,Write=True)
-        
+        self.Slots=Slots
         self.Pins=Pins
 
         super().__init__(self.__class__.__name__,Name, ReferenceId)
