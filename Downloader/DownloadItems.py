bde9339bb74725067db1ea98777d0081145f6620
Fireblade2534 <Fireblade5234@gmail.com>
2024-11-18 19:15:51 +0000

    Started work on network channels

--- Downloader/DownloadItems.py
+++ Downloader/DownloadItems.py
@@ -1,4 +1,13 @@
 #Base url https://stationeers-wiki.com/index.php?title=Category:Items
+
+"""
+GetLinks (First is for the next page):
+#mw-pages a
+
+
+"""
+
+
 from bs4 import BeautifulSoup
 import requests
 
