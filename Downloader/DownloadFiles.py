7b987139a985122b64a949966c54ce96ccb8a181
Fireblade2534 <Fireblade5234@gmail.com>
2024-11-25 19:18:22 +0000

    Downloaded new device json and fixed some of the tests that depended on settings that were in the old devices.json

--- Downloader/DownloadFiles.py
+++ Downloader/DownloadFiles.py
@@ -1,14 +1,43 @@
+import os, sys
+sys.path.insert(0, os.getcwd()) 
+
 import requests
 import json
+import Configs.Constants as Constants
+
 def DeviceParser(Json):
     Output={}
     
     for X in Json["data"]:
-        DeviceInfo={"Fields":{}}
+        DeviceInfo={"Name":X["Title"],"Fields":{},"ConnectionsNumber":len(X["connections"]), "Pins": {"Number":X["deviceConnectCount"]}, "SlotType":"Type_None", "Slots": [],"Properties": {
+      "RunCode": False,
+      "Stack": {
+        "Enabled": False,
+        "Length": 512
+      }
+    },"Variables": {
+      "Code": "NONE"
+    }}
 
         for Y in X["logics"]:
+            if Y["name"] not in Constants.DO_NOT_DEFINE_FIELDS:
+                Perms={"Value":0,"Read":"Read" in Y["permissions"],"Write":"Write" in Y["permissions"]}
+                if Y["name"] in Constants.OVERRIDE_FIELD_DEFAULT:
+                    Perms["Value"]=Constants.OVERRIDE_FIELD_DEFAULT[Y["name"]]
+                DeviceInfo["Fields"][Y["name"]]=Perms
+            
+        for Y in X["slots"]:
+            DeviceInfo["Slots"].append({"Type":f"Type_{Y['SlotType']}","Name":Y["SlotName"],"Index":Y["SlotIndex"]})
 
+        if X["hasChip"]:
+            DeviceInfo["Properties"]["RunCode"]=True
 
+        if "hasLogicInstructions" in X["tags"] or X["hasChip"]:
+            DeviceInfo["Properties"]["Stack"]["Enabled"]=True
+        
+        Output[X["PrefabName"]]=DeviceInfo
+    json.dump(Output,open("Configs/Devices.json","w"),indent=4)
+    return 1
 
 def Main():
     MenuOptions={"device":{"Url":"https://assets.ic10.dev/languages/EN/devices.json","Parser":DeviceParser}}
